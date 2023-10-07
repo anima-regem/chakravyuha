b551dd6eaae3058212ae1fae427748288bb1d0bb
Bijil <connectwithbijilkp@gmail.com>
2023-10-08 00:50:29 +0530

    initial commit

---
+++ src/components/particles/DataLists.tsx
@@ -0,0 +1,240 @@
+export const NavLinks = [
+    {
+        name: "Destination",
+        url: "/"
+    },
+    {
+        name: "Hotels",
+        url: "/"
+    },
+    {
+        name: "Flights",
+        url: "/"
+    },
+    {
+        name: "Bookings",
+        url: "/"
+    }
+
+];
+
+export const NavButtons = [
+    {
+        name: "Login",
+        url: "/"
+    },
+    {
+        name: "Signup",
+        url: "/"
+    }
+
+];
+
+
+export const HeroTexts = {
+    //firstText: "BEST DESITNATIONS AROUND THE WORLD",
+    firstText: "Your Sustainable Journey Begins Here",
+    //secondText: "Travel, enjoy and live a new and full life.Sustainability is the New Luxury",
+    secondText: "Sustainability is the New Luxury",
+    //thirdText: "Built Wicket longer admire do barton vantity itself do in it. Preferred to sportsmen it engrossed listening. Park gate sell they west hard for the.",
+    thirdText: "Plan an eco-adventure that leaves memories, not footprints",
+    firstButton: "Find out more",
+    secondButton: 'Play Demo'
+}
+
+
+export const ServiceTexts = {
+    firstText: "CATEGORY",
+    secondText: "We Offer Best Services",
+    cards: [
+        {
+            firstText: "Sustainable tour packages",
+            secondText: "Discover our eco-friendly travel experiences with our sustainable tour packages."
+        },
+        {
+            firstText: "Sustainable transportation ",
+            secondText: "Ensuring your journeys are both convenient and environmentally responsible",
+        },
+        {
+            firstText: "Sustainable stay",
+            secondText: "Experience a sustainable stay that harmonizes comfort with eco-consciousness during your travels."
+        },
+        {
+            firstText: "Carbon Footprint Calculator",
+            secondText: "Calculate your carbon footprint and discover ways to reduce your environmental impact through eco-friendly choices"
+        }
+    ]
+}
+
+export const TopDestinationTexts = {
+    firstText: "Top selling",
+    secondText: "Top Destinations",
+    cards: [
+        {
+            country: "Munnar",
+            price: "5.42k",
+            duration: "10 days trip"
+        },
+        {
+            country: "Paithalmala",
+            price: "4.2k",
+            duration: "12 days trip"
+        },
+        {
+            country: "Nilambur Nedumkayam",
+            price: "15k",
+            duration: "26 days trip"
+        },
+        {
+            country: "Chinnar Wildlife Sanctuary",
+            price: "4.5k",
+            duration: "8 days trip"
+        },
+        {
+            country: "Periyar Tiger Reserve",
+            price: "3.9k",
+            duration: "14 days trip"
+        },
+        {
+            country: "Thommankuthu Waterfalls",
+            price: "9.2k",
+            duration: "17 days trip"
+        },
+    ]
+}
+
+export const BookingStepsTexts = {
+    firstText: "Easy and Fast",
+    secondText: "Book your next trip in 3 easy steps",
+    listOfSteps: [
+        {
+            text: "Select your next destination from our extensive list and start your next unforgettable journey"
+        },
+        {
+            text: "You can easily make payments using a variety of methods, including cards, UPI, and more"
+        },
+        {
+            text: "Reach Pickup point  on Selected Date"
+        }
+    ],
+    cardOne:
+    {
+        name: "Trip To Eravikulam",
+        date: "07-10 October  |  by Bijil",
+        people: "32 people going"
+    },
+    cardTwo: {
+        status: "Ongoing",
+        destination: "Trip to Ponmudi",
+        completion: "50% Completed"
+    }
+
+}
+
+export const TestimonialTexts = {
+    firstText: "TESTIMONIALS",
+    secondText: "What people say about Us.",
+    feedBacks: [
+        {
+            text: "Highly impressed with the variety of sustainable tours available on this platform. Not only did I have a fantastic time, but I also felt like I was doing my part for the planet. A win-win for eco-conscious travelers like me!",
+            person: "Abhay c",
+            location: "Kannur,kerala"
+        },
+        {
+            text: "Kudos to this platform for making sustainable travel accessible and enjoyable. I've had the pleasure of exploring breathtaking destinations with responsible tour operators, and I appreciate the opportunity to support eco-conscious tourism",
+            person: "Amal George",
+            location: "Thrissur,Kerala"
+        },
+        {
+            text: "Booking through you was very easy and made our lives so much easier. I have nothing bad to say! ",
+            person: "Niyuktha",
+            location: "Eranakulam,Kerala"
+        },
+        {
+            text: "I've been using this sustainable tour platform for a while now, and it has completely transformed my travel experiences. From eco-friendly accommodations to responsible excursions, it's a game-changer for conscious travelers seeking meaningful adventures",
+            person: "Sherin Rafi",
+            location: "Wayanad,Kerala"
+        },
+    ],
+}
+
+export const NewsletterTexts = {
+    firstText: "Subscribe to get information, latest news and other interesting offers about Jadoo.",
+    placeholderText: "Your email",
+    buttonText: "Subscribe"
+}
+
+export const FooterTexts = {
+    underLogoText: "Book your trip in minutes, get full control for much longer.",
+    quickLinks: {
+        caption: "Quick Links",
+        links: [
+            {
+                name: "Destination",
+                url: "/"
+            },
+            {
+                name: "Hotels",
+                url: "/"
+            },
+            {
+                name: "Flights",
+                url: "/"
+            },
+            {
+                name: "Bookings",
+                url: "/"
+            },
+            {
+                name: "Login",
+                url: "/"
+            },
+            {
+                name: "Signup",
+                url: "/"
+            }
+        ]
+    },
+    contacts: {
+        caption: "Contacts",
+        links: [
+            {
+                name: "FAQs",
+                url: "/"
+            },
+            {
+                name: "Help",
+                url: "/"
+            },
+            {
+                name: "Policies",
+                url: "/"
+            },
+            {
+                name: "Terms & Conditions",
+                url: "/"
+            }
+        ]
+    },
+    more: {
+        caption: "More",
+        links: [
+            {
+                name: "Career",
+                url: "/"
+            },
+            {
+                name: "Airlines",
+                url: "/"
+            },
+            {
+                name: "Airline Fees",
+                url: "/"
+            },
+            {
+                name: "Low Fare Tips",
+                url: "/"
+            }
+        ]
+    }
+}
